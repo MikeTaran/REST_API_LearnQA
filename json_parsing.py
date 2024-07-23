c82da40e7cf73499d8f8ba9e9d7a39dd6ce14ad3
Bear-Asus <mtaran@ukr.net>
2024-07-23 19:44:40 +0300

    json parsing

--- json_parsing.py
+++ json_parsing.py
@@ -1,7 +1,14 @@
 import json
+from json.decoder import JSONDecodeError
 
 json_text = ('{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},'
              '{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}')
+# json_text = "hello all"
+try:
+    obj = json.loads(json_text)
+    print(obj['messages'][1]["message"])
+    print(obj['messages'][1]["timestamp"])
 
-obj = json.loads(json_text)
-print(obj['messages'][1])
+except JSONDecodeError:
+    print("json_text is not json format")
+
