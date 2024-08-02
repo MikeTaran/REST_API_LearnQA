da9f3eb944447d3a7ee1d82cb7ef075c1677ec1a
Bear-Asus <mtaran@ukr.net>
2024-08-03 01:31:50 +0300

    Ex16: TestUserGet

--- lib/assertion.py
+++ lib/assertion.py
@@ -23,6 +23,32 @@ class Assertion:
         assert name in response_dict, f"The key: {name} is not in response JSON"
 
     @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_dict = response.json()
+        except json.decoder.JSONDecoder:
+            assert False, f"The response is not in JSON format. Response text is: {response.text}"
+        for name in names:
+            assert name in response_dict, f"The key: {name} is not in response JSON"
+
+    @staticmethod
+    def assert_json_has_not_keys(response: Response, names: list):
+        try:
+            response_dict = response.json()
+        except json.decoder.JSONDecoder:
+            assert False, f"The response is not in JSON format. Response text is: {response.text}"
+        for name in names:
+            assert name not in response_dict, f"The key: {name} shouldn't in response JSON, but it is present"
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name):
+        try:
+            response_dict = response.json()
+        except json.decoder.JSONDecoder:
+            assert False, f"The response is not in JSON format. Response text is: {response.text}"
+        assert name not in response_dict, f"The key: {name} shouldn't  in response JSON, but it is present"
+
+    @staticmethod
     def assert_response_code_status(response: Response,expected_code):
         assert response.status_code == expected_code, (f"Unexpected status code! Expected: {expected_code}, "
                                                        f"actual: {response.status_code}")
